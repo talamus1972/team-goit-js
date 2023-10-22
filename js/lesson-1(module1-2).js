2bed1b78ce3a0b3874c4845b549e4538eff95074
talamus1972 <talamus1972@gmail.com>
2023-10-22 15:26:39 +0300

    11

---
+++ js/lesson-1(module1-2).js
@@ -0,0 +1,11 @@
+// TODO: № 1
+// Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
+
+// let a = 10;
+// if (a === 5) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// console.log(a === 5 ? true : false);
