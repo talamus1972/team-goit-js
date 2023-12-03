4768b6fd89e7ce33fde197e080019c3dc97c2a0c
talamus1972 <talamus1972@gmail.com>
2023-12-03 15:11:41 +0200

    tttt

---
+++ js/lesson-4(module7).js
@@ -0,0 +1,24 @@
+//TODO: № 1 ==============================================
+// Натискання на кнопку "SHOW ME" має виводити значення з 
+// поля введення(дивіться на елементи в html - розмітці)
+
+// const btn = document.querySelector('#alertButton');
+// const inputText = document.querySelector('#alertInput');
+
+
+// btn.addEventListener('click', () => {
+//     // inputText.value
+//     console.log(inputText.value);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
