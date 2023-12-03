b26b1122bd084047e3265281fae3810facb268ea
Natali25072023 <infkab2@gmail.com>
2023-12-03 15:30:45 +0200

    task-2

--- js/lesson-4(module7).js
+++ js/lesson-4(module7).js
@@ -11,8 +11,20 @@
 //     console.log(inputText.value);
 // })
 
+//TODO: № 2 ==============================================
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
 
+// const btnEl = document.querySelector('#swapButton');
+// const inputLeft = document.querySelector('#leftSwapInput');
+// const inputRight = document.querySelector('#rightSwapInput');
 
+// btnEl.addEventListener('click', () => {
+//     const textInput = inputLeft.value;
+//     inputLeft.value = inputRight.value;
+//     inputRight.value = textInput;
+ 
+// })
 
 
 
